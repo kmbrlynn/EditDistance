986d0c8d7c24b8ccfb4521e57b3a64ef1405d386
kmbrlynn <douglas.kimberly.a@gmail.com>
2015-03-09 11:36:55 -0400

    add function stubs

--- ED.cpp
+++ ED.cpp
@@ -6,3 +6,32 @@ ED::ED(std::string s, std::string t)
 
 
 }
+
+int ED::penalty(char a, char b)
+{
+	int penalty = 0;
+
+	return penalty;
+}
+
+int ED::min(int a, int b, int c)
+{
+	int min = 0;
+
+	return min;
+}
+
+int ED::OptDistance()
+{
+	int opt_distance = 0;
+
+	return opt_distance;
+	
+}
+
+std::string ED::Alignment()
+{
+	std::string alignment;
+
+	return alignment;
+}
--- ED.hpp
+++ ED.hpp
@@ -8,6 +8,11 @@ public:
 
 private:
 
+	int penalty(char, char);
+	int min(int, int, int);
+	int OptDistance();
+	std::string Alignment();
+
 	std::string _s;
 	std::string _t;
 
