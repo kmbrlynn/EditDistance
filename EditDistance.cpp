f55ea8dbf67f2d56d7862487e6a9c772b89ffc11
kmbrlynn <douglas.kimberly.a@gmail.com>
2015-03-12 18:25:57 -0400

    enable piping from file to stdin - disable args to main

--- EditDistance.cpp
+++ EditDistance.cpp
@@ -5,34 +5,32 @@
 
 int main(int argc, char* argv[])
 {
-	std::string s = argv[1];
-	std::string t = argv[2];
-	
+	std::string s; // corresponds to: x, rows of matrix, i indices
+	std::string t; // corresponds to: y, columns of matrix, j indices
+
+	std::cin >> s; // first string in file
+	std::cin >> t; // second string in file
+
+	int edit_distance;	
 	std::string edit_distance_path;
 
 	sf::Clock clock;
 	sf::Time time;
 
 	ED my_ed(s,t);
-	
-	// lay out a matrix
-	my_ed.OptDistance();
+
+	// lay out a matrix and get the optimal edit distance
+	edit_distance = my_ed.OptDistance();
 
 	// recover the path of the optimal edit distance
 	edit_distance_path = my_ed.Alignment();
 
-	// print each char and the cost of its operation
-	std::cout << std::endl << edit_distance_path << std::endl;
+	std::cout << "Edit distance = " << edit_distance << std::endl;
+	std::cout << edit_distance_path << std::endl;
 
 	time = clock.getElapsedTime();
 	std::cout << "Execution time is: " << time.asSeconds() << " seconds.";
 	std::cout << std::endl << std::endl;
-
-
-
-
-
-
 
 	return 0;
 }
